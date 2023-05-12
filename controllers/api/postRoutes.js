86c74c1b4de9312097cb7ad48a7272f0a601ed3a
Eibon <ninetimes64@gmail.com>
2023-05-12 14:45:13 -0500

    created controllers

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,55 @@
+const router
+const { Post, User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        });
+
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+);
+
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedPost = await Post.update(
+            {
+                title: req.body.title,
+                content: req.body.content
+            },
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        );
+
+        res.status(200).json(updatedPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
+
+module.exports = router;
